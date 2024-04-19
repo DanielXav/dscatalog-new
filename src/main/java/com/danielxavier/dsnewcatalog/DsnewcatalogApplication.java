6985f4f29ec9c97d185341b02788f5fac7d97ff6
Daniel Xavier <dxavier194@gmail.com>
2024-04-18 21:01:12 -0300

    Project Created

---
+++ src/main/java/com/danielxavier/dsnewcatalog/DsnewcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.danielxavier.dsnewcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsnewcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsnewcatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielxavier/dsnewcatalog/DsnewcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielxavier.dsnewcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsnewcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
